20788f6afe095883542f516faf5616cbef551c3a
Shivam kumar <sk8612181@gmail.com>
2026-03-11 11:54:30 +0530

    feat(api): implement http server and rate-limiting middleware pattern

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,9 +1,36 @@
+// File: cmd/gateway/main.go
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+
+	"github.com/shivamk01here/distributed-gateway/internal/api"
+	"github.com/shivamk01here/distributed-gateway/internal/limiter"
 )
 
+// dummyBackendService simulates the core service we are trying to protect.
+func dummyBackendService(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Success! You reached the protected backend service.\n")
+}
+
 func main() {
-	log.Println("Gateway started")
+	log.Println("Initializing Rate Limiter...")
+	tb := limiter.NewTokenBucket(5, 1.0)
+
+	log.Println("Setting up HTTP routes...")
+	protectedHandler := api.RateLimitMiddleware(tb, dummyBackendService)
+
+	// Register the route
+	http.HandleFunc("/api/data", protectedHandler)
+
+	port := ":8080"
+	log.Printf("Starting Distributed API Gateway on port %s...\n", port)
+
+	// Start the server
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
